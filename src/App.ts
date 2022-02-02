dfc1823a12909f270f02a461f53657de55c19db2
worldwarmworm <st.valeriq@gmail.com>
2022-02-02 20:17:28 +0700

    add logger service

--- src/App.ts
+++ src/App.ts
@@ -1,14 +1,17 @@
 import express, { Express } from "express";
 import { Server } from "http";
+import { LoggerService } from "./logger/LoggerService";
 
 export class App {
   app: Express;
   server: Server;
   port: number;
+  logger: LoggerService;
 
-  constructor() {
+  constructor(logger: LoggerService) {
     this.app = express();
     this.port = 8000;
+    this.logger = logger;
   }
 
   useRoutes() {
@@ -18,6 +21,6 @@ export class App {
   public async init() {
     this.useRoutes();
     this.server = this.app.listen(this.port)
-    console.info(`Server started at http://localhost:${this.port}`);
+    this.logger.log(`Server started at http://localhost:${this.port}`);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
-import { App } from "./app";
+import { App } from "./App";
+import { LoggerService } from "./logger/LoggerService";
 
 
 async function bootstrap() {
-  const app = new App();
+  const app = new App(new LoggerService);
   await app.init();
 }
 
